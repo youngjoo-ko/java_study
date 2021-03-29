a9784fb0b8b649e65dc5ed6068d1531325107e31
youngjoo-ko <elelelel77@naver.com>
2021-03-29 22:31:59 +0900

    java_study

--- src/OOP/class_1.java
+++ src/OOP/class_1.java
@@ -15,8 +15,8 @@ public class class_1 {
 		c2.kind = "Heart"; // 인스턴스 변수 값 수정
 		c2.number = 4;
 		
-		c1.width = 50; // 클래스 변수 값 수정
-		c1.height = 60; // c1, c2가 공유하는 변수이므로 c2도 수정됨.
+		Card.width = 50; // 클래스 변수 값 수정
+		Card.height = 60; // c1, c2가 공유하는 변수이므로 c2도 수정됨.
 		
 		System.out.println("클래스 변수 width, height 값을 50, 60으로 변경합니다.");
 		System.out.printf("c1의 무늬 = %s, 숫자 = %d, 너비= %d, 높이 = %d%n", c1.kind, c1.number, Card.width, Card.height);
