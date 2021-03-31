a5487bc593e992ae3f0a8d83c852184c08d2a9e9
youngjoo-ko <elelelel77@naver.com>
2021-03-31 21:48:08 +0900

    java_study

--- src/OOP/reference_param.java
+++ src/OOP/reference_param.java
@@ -3,9 +3,7 @@ package OOP;
 class Data2 {int x;}
 
 public class reference_param {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Data2 d = new Data2(); // 객체 생성, 참조변수 d에 객체의 주소 저장
 		d.x = 10; //객체가 가진 멤버변수 x에 10 저장
 		System.out.println("main x = " + d.x);
---
+++ src/OOP/reference_return.java
@@ -0,0 +1,22 @@
+package OOP;
+
+class Data3 {int x;}
+
+public class reference_return {
+
+	public static void main(String[] args) {
+		Data3 d = new Data3(); // 객체 생성, 참조변수 d에 객체의 주소 저장
+		d.x = 10; //객체가 가진 멤버변수 x에 10 저장
+
+		Data3 d2 = copy(d); // 객체의 주소 복사후 매개변수로 넘김
+		System.out.println("d.x = " + d.x);
+		System.out.println("d2.x = " + d2.x);
+	}
+		
+		static Data3 copy(Data3 d) { 
+			Data3 tmp = new Data3();
+			tmp.x = d.x; // 새로운 참조변수 tmp의 멤버변수 x에 d의 멤버변수 x를 복사함
+			return tmp;// 복사한 객체의 주소 반환
+		}
+
+}
