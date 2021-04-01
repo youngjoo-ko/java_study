249cacc174a484075d529fe3abf8787e061efe91
youngjoo-ko <elelelel77@naver.com>
2021-04-01 21:14:55 +0900

    java_study

---
+++ src/OOP/q6_1.java
@@ -0,0 +1,34 @@
+package OOP;
+
+class Student {
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+	
+	Student(String name, int ban, int no, int kor, int eng, int math){
+		this.name = name;
+		this.ban = ban;
+		this.no = no;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+
+
+	String info() {
+		return name + "," + no + "," + kor + "," + eng + "," + math
+				+ "," + (kor+eng+math) + "," + (int)((kor+eng+math)/3);
+	}
+}
+
+class q6_1{
+	public static void main(String[] args) {
+		Student s = new Student("홍길동", 1, 2, 100, 40, 60);
+		
+		String str = s.info();
+		System.out.println(str);
+	}
+}
