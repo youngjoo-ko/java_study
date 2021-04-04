0d5acb60d34b8f3678d4bb0fefe84b6fefdf650b
youngjoo-ko <elelelel77@naver.com>
2021-04-04 23:09:45 +0900

    java_study

---
+++ src/OOP/q6_18.java
@@ -0,0 +1,31 @@
+package OOP;
+
+public class q6_18 {
+	public static boolean isNumber(String str) {
+		if(str==null || str.equals("")) return false;
+
+		boolean b = false;
+		for(int i=0; i<str.length(); i++) {
+			char s = str.charAt(i);
+			System.out.println(s);
+			if('0'<=s && s<='9') {
+				b = true; 
+			}
+			else {
+				b = false;
+				break;
+			}
+		}
+		return b;
+	}
+	
+	public static void main(String[] args) {
+		String str = "123";
+		System.out.println(isNumber(str));
+
+		String str2 = "ddq";
+		System.out.println(isNumber(str2));
+
+	}
+
+}
