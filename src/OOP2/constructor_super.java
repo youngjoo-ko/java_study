af82798fd28f4cc6b07cff5fbbef1029cfb49a37
youngjoo-ko <elelelel77@naver.com>
2021-04-05 16:39:54 +0900

    java_study

---
+++ src/OOP2/constructor_super.java
@@ -0,0 +1,28 @@
+package OOP2;
+
+public class constructor_super {
+
+	public static void main(String[] args) {
+		Point3D p = new Point3D(1,2,3);
+		System.out.println (p.x + "," + p.y + "," + p.z);
+	}
+}
+
+class Po{
+	int x,y;
+	
+	Po(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+
+class Point3D extends Po{
+	int z;
+	
+	Point3D(int x, int y, int z){
+		super(x,y); // Point2(int x, int y) 생성자 호출
+		this.z = z;
+	}
+}
