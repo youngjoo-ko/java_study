fa701ebbd776334e49d412cc153105ceabe892a9
youngjoo-ko <elelelel77@naver.com>
2021-04-07 22:33:26 +0900

    java_study

---
+++ src/OOP2/static_import.java
@@ -0,0 +1,14 @@
+package OOP2;
+
+import static java.lang.System.out;
+import static java.lang.Math.*; 
+
+
+public class static_import {
+	public static void main(String[] args) {
+		out.println(random());
+		
+		out.println("Math.PI" + PI);
+	}
+
+}
