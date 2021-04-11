6886fa0876e896be40b173ec8641390bf90fe5f1
youngjoo-ko <elelelel77@naver.com>
2021-04-11 18:52:34 +0900

    java_study

--- src/OOP2/q7_4.java
+++ src/OOP2/q7_4.java
@@ -2,34 +2,41 @@ package OOP2;
 
 class MyTv {
 	// private 같은 클래스 내부에서만 접근 가능
-	private boolean isPowerOn;
+	//private boolean isPowerOn;
 	private int channel;
 	private int volume;
+	private int prevCh;
 	
 	final int MAX_VOLUME = 100;
 	final int MIN_VOLUME = 0;
 	final int MAX_CHANNEL = 100;
 	final int MIN_CHANNEL = 1;
 	
-	void setVolume(int volume) {
+	public void setVolume(int volume) {
 		if(volume > MAX_VOLUME || volume < MIN_VOLUME) {
 			return;
 		}
 		this.volume = volume;
 	}
 	
-	int getVolume() {
+	public int getVolume() {
 		return volume;
 	}
 	
-	void setChannel(int channel) {
+	public void setChannel(int channel) {
 		if(channel > MAX_CHANNEL || channel < MIN_CHANNEL) {
 			return;}
+		
+		prevCh = this.channel;
 		this.channel = channel;
 	}
 	
-	int getChannel() {
+	public int getChannel() {
 		return channel;
+	}
+	
+	public void gotoPrevCh() {
+		setChannel(prevCh);
 	}
 }
 
@@ -43,6 +50,11 @@ public class q7_4 {
 		System.out.println("channel=" + t.getChannel());
 		t.setVolume(20);
 		System.out.println("volume=" + t.getVolume());
+		t.setChannel(100);
+		System.out.println("channel=" + t.getChannel());
+		t.gotoPrevCh();
+		System.out.println("channel=" + t.getChannel());
+
 	}
 
 }
